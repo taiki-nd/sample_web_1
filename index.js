ea32acdf6f9a4e78d30cd5262b6986d801b00a2c
taiki-nd <taiki.nd.real@gmail.com>
2021-06-03 15:55:58 +0900

    hamberger

--- index.js
+++ index.js
@@ -1,58 +1,15 @@
 $(function(){
-    /*=================================================
-    スマホメニュー
-    ===================================================*/
-    // ハンバーガーメニューのクリックイベント
-    // 解説は、「中級編：ストアサイト（インテリア）」参照
-    $('.toggle_btn').on('click', function() {
-      if (!$('.header').hasClass('open')) {
-        $('.header').addClass('open');
-      } else {
-        $('.header').removeClass('open');
-      }
-    });
-  
-    $('.mask').on('click', function() {
-      $('.header').removeClass('open');
-    });
-  
-    /*=================================================
-    スムーススクロール
-    ===================================================*/
-    // ページ内リンクのイベント
-    $('a[href^="#"]').click(function(){
-      // リンクを取得
-      let href= $(this).attr("href");
-      // ジャンプ先のid名をセット
-      let target = $(href == "#" || href == "" ? 'html' : href);
-      // トップからジャンプ先の要素までの距離を取得
-      let position = target.offset().top;
-      // animateでスムーススクロールを行う
-      // 600はスクロール速度で単位はミリ秒
-      $("html, body").animate({scrollTop:position}, 600, "swing");
-      return false;
-    });
-  
-  
-    /*=================================================
-    スクロール時の画像フェード表示
-    ===================================================*/
-    // スクロール時のイベント
-    $(window).scroll(function() {
-      // fadeinクラスに対して順に処理を行う
-      $('.fadein').each(function() {
-        // スクロールした距離
-        let scroll = $(window).scrollTop();
-        // fadeinクラスの要素までの距離
-        let target = $(this).offset().top;
-        // 画面の高さ
-        let windowHeight = $(window).height();
-        // fadeinクラスの要素が画面下にきてから200px通過した
-        // したタイミングで要素を表示
-        if (scroll > target - windowHeight + 200) {
-          $(this).css('opacity','1');
-          $(this).css('transform','translateY(0)');
-        }
-      });
-    });
+
+  $('.toggle_btn').on('click', function(){
+    if (!$('#header').hasClass('open')){
+      $('#header').addClass('open');
+    }else{
+      $('#header').removeClass('open');
+    }
   });
+
+  $('#mask').on('click', function(){ 
+    $('#header').removeClass('open');
+  });
+
+});
