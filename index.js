90480a5aa077887a9cbf2c8afbb2b0a503633f53
taiki-nd <taiki.nd.real@gmail.com>
2021-06-03 17:54:34 +0900

    scroll

--- index.js
+++ index.js
@@ -12,4 +12,15 @@ $(function(){
     $('#header').removeClass('open');
   });
 
+
+  $('a[href^="#"]').click(function(){
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, 600, "swing");
+    return false;
+  });
+
+
 });
+
